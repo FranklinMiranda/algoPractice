0130854ea60fe8cf11e74be14cb73b1bed23708f
FranklinMiranda <fdm61344@gmail.com>
2023-05-15 19:06:45 -0400

    LoopUpdate

--- nLoops.js
+++ nLoops.js
@@ -7,28 +7,33 @@ const n = 3;
 const counter = [];
 
 function nestedForLoop(n, k) {
-  for (let i = 1; i <= k; i++) {
+  for (let i = k; i > 0; i--) {
     if (n > 0) {
-      if (n === 1) {counter.push(1)}
-      nestedForLoop(n - 1, k);
+      console.log(n, i);
+
+      if (n === 1) {
+        counter.push(1);
+      }
+
+      nestedForLoop(n - 1, i);
     }
   }
 
   return;
 }
 
-nestedForLoop(4, 9);
+nestedForLoop(n, n);
 
-console.log(counter.length)
+console.log(counter.length);
 
 const count = [];
 
-for (let i = 1; i < 3; i++) {
-  for (let j = 1; j < 3; j++) {
-    for (let k = 1; k < 3; k++) {
-      for (let l = 1; l < 3; l++) {
-        for (let g = 1; g < 3; g++) {
-          for (let f = 1; f < 3; f++) {
+for (let i = 1; i < 9; i++) {
+  for (let j = i + 1; j < 9; j++) {
+    for (let k = j + 1; k < 9; k++) {
+      for (let l = k + 1; l < 9; l++) {
+        for (let g = l + 1; g < 9; g++) {
+          for (let f = g + 1; f < 9; f++) {
             count.push(1);
           }
         }
--- partialFactorial.js
+++ partialFactorial.js
@@ -2,43 +2,30 @@
 // Date : 5/14/2023
 // Description: An algorithm to find a partial factorial of a array
 
-const arr = [1, 2, 3, 4, 5];
+const arr = [1, 2, 3, 4, 5, 6];
 
 function combinations(arr, resultLength) {
   const combinationsArr = [];
   const array = new Array(...arr);
-  const arrayLength = array.length;
   const n = resultLength;
   let loopCounter = 0;
 
   function loop(n, k) {
-    for (let i = 0; i < k; i++) {
+    for (let i = k; i > 0; i--) {
       if (n > 0) {
         if (n === 1) {
           loopCounter += 1;
-          const permutation = [];
-
-          for (let i = 0; i < resultLength; i++) {
-            if (i === 0) {
-              const index = (loopCounter - 1) % k;
-              permutation.unshift(array[index]);
-            } else {
-              const index = Math.trunc((loopCounter - 1) / k ** i) % k;
-
-              permutation.unshift(array[index]);
-            }
-          }
-          combinationsArr.push(permutation);
         }
-        loop(n - 1, k);
+        loop(n - 1, i - 1);
       }
     }
 
     return;
   }
 
-  loop(n, arrayLength);
+  loop(n, array.length);
 
+  console.log(loopCounter);
   console.log(JSON.stringify(combinationsArr));
 
   return combinationsArr;
