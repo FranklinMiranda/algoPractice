42e0d3c37ede713c5d62c675fae87c59a44259ca
FranklinMiranda <fdm61344@gmail.com>
2023-06-10 11:10:36 -0400

    Update to Euclidean Algorithm

--- Recursion/DirectRecursion/TailRecursion/tailRecursionEuclideanAlgorithm.js
+++ Recursion/DirectRecursion/TailRecursion/tailRecursionEuclideanAlgorithm.js
@@ -1,11 +1,11 @@
 // Name : Franklin Miranda
 // Date : 6/10/2023
-// Description: The Euclidean algorithm to calculate the Greatest Common Divisor (GCD) of two numbers
+// Description: The Euclidean algorithm to calculate the Greatest Common Divisor (GCD) of two positive integers
 
 function gcd(num1, num2) {
   if (num1 === num2) {
     return num1;
-  }
+  } else if (num1 < 1 || num2 < 1) {return}
 
   if (num1 > num2) {
     num1 = num1 - num2;
@@ -16,4 +16,4 @@ function gcd(num1, num2) {
   return gcd(num1, num2);
 }
 
-console.log(gcd(105, 252))
+console.log(gcd(-10, 5))
