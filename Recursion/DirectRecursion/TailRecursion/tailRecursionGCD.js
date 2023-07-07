ee1c05e7a1423e704ac411e9ef8ac2520e9ce599
FranklinMiranda <fdm61344@gmail.com>
2023-07-06 20:51:15 -0400

    Update to Tail Recursion GCD

--- Recursion/DirectRecursion/TailRecursion/tailRecursionGCD.js
+++ Recursion/DirectRecursion/TailRecursion/tailRecursionGCD.js
@@ -2,7 +2,7 @@
 // Date : 7/6/2023
 // Description: The Euclidean algorithm to calculate the Greatest Common Divisor (GCD) of any sequence of numbers
 
-let numArr = [72, 96, 120, 24];
+let numArr = [6, 96, 120, 24];
 
 function greatestCommonDivisor(array, gcd = array.shift()) {
   if (array.length === 0) {
